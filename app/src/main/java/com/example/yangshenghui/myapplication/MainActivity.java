0d4a8549359c5ff95514ad9a820cc0bbe2e59889
YangShenghui <810821004@qq.com>
2016-12-26 23:08:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yangshenghui/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yangshenghui.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO finish this code
+        setContentView(R.layout.activity_main);
+    }
+}
